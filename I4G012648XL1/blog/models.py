7786f1cd3c7b7a1735c0b33d282edfcd56a846b4
praiseolawoore <olawoorejohn@gmail.com>
2022-06-18 15:20:16 +0100

    my django models task

--- I4G012648XL1/blog/models.py
+++ I4G012648XL1/blog/models.py
@@ -1,14 +1,13 @@
-# from django.db import models
-# from django.contrib.auth.models import User
-# from django.utils import timezone
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# # Create your models here.
-# class Post(models.Model):
-#     title = models.CharField(max_length=200)
-#     text = models.TextField(null=True)
-#     author = models.ForeignKey(User, on_delete= models.SET_NULL, null=True)
-#     created_date = models.DateTimeField(default=timezone.now)
-#     published_date = models.DateTimeField(default=timezone.now)
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField(null=True)
+    author = models.ForeignKey(User, on_delete= models.SET_NULL, null=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(default=timezone.now)
 
 
-print("Welcome .............................................")
